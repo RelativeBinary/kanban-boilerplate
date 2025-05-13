5cf66983bf5435e673675d6d677d7f4f47c50ee3
RelativeBinary <patrick.bornay@gmail.com>
2025-05-14 09:05:14 +1000

    server refresh

---
+++ server-old/src/index.ts
@@ -0,0 +1,41 @@
+// index.js
+import express from "express";
+import supabase from "../src/config/supabaseClient.js";
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT ?? "2000";
+const corsOptions = {
+  origin: `http://localhost:3001`, 
+  credentials: false,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World! From Railway!");
+  console.log("Response sent");
+});
+
+app.router.get('/tasks', async (require, res) => {
+  try {
+    const { data, error } = await supabase 
+    .from('tasks')
+    .select();
+
+    if (error) {
+      console.log('error', error);
+      throw error;
+    }
+
+    res.status(200).json(data);
+  } catch {
+    console.error('Error fetchign users');
+    res.status(500).json({ error: 'Error message' });
+  }
+});
+
+app.listen(port, () => {
+  console.log('supabase', supabase);
+  console.log(`Example app listening on port ${port}`);
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,41 +1,17 @@
-// index.js
-import express from "express";
-import supabase from "./config/supabaseClient";
-import cors from 'cors';
+import express from 'express';
+import { config } from 'dotenv';
+
+
+config()
 
 const app = express();
-const port = process.env.PORT ?? "2000";
-const corsOptions = {
-  origin: `http://localhost:3001`, 
-  credentials: false,            //access-control-allow-credentials:true
-  optionSuccessStatus: 200
-}
-app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World! From Railway!");
-  console.log("Response sent");
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_, res) => {
+  res.send("<h1>Hello! World!");
 });
 
-app.router.get('/tasks', async (require, res) => {
-  try {
-    const { data, error } = await supabase 
-    .from('tasks')
-    .select();
-
-    if (error) {
-      console.log('error', error);
-      throw error;
-    }
-
-    res.status(200).json(data);
-  } catch {
-    console.error('Error fetchign users');
-    res.status(500).json({ error: 'Error message' });
-  }
-});
-
-app.listen(port, () => {
-  console.log('supabase', supabase);
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
