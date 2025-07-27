817091210ab7696f8d4c39dded84b3672c38e31a
RelativeBinary <patrick.bornay@gmail.com>
2025-07-27 18:35:08 +1000

    remove console log

--- webui/src/supabaseClient.ts
+++ webui/src/supabaseClient.ts
@@ -3,7 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL ?? 'ERROR NO SUPABASE URL DEFINED';
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY ?? 'ERROR NO SUPABASE API KEY DEFINED';
-console.log('supabaseURL', supabaseUrl);
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
