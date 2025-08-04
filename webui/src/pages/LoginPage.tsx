3f41e5b6b3519a84ecd9e86f846bc3420bee6116
RelativeBinary <patrick.bornay@gmail.com>
2025-08-04 11:03:40 +1000

    add redirect url configuration

--- webui/src/pages/LoginPage.tsx
+++ webui/src/pages/LoginPage.tsx
@@ -2,11 +2,13 @@ import { Auth } from "@supabase/auth-ui-react"
 import supabase from "../supabaseClient"
 import { ThemeSupa } from "@supabase/auth-ui-shared"
 
+const redirectUrl = import.meta.env.VITE_REDIRECT_URL ?? 'ERROR NO REDIRECT URL DEFINED';
+
 export const LoginPage = () => {
   return (
     <div>
       <h1>Welcome! Please sign in</h1>
-      <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+      <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} redirectTo={redirectUrl} />
     </div>
   )
 }
